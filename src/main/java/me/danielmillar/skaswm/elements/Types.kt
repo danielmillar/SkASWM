41f754eb71d790fe2db3ca49e6f71325e2dde504
Daniel Millar <millargdaniel@gmail.com>
2024-06-16 20:34:14 +0100

    Introduce parser for Slime Property Map

--- src/main/java/me/danielmillar/skaswm/elements/Types.kt
+++ src/main/java/me/danielmillar/skaswm/elements/Types.kt
@@ -2,7 +2,10 @@ package me.danielmillar.skaswm.elements
 
 import ch.njol.skript.classes.ClassInfo
 import ch.njol.skript.classes.EnumClassInfo
+import ch.njol.skript.classes.Parser
+import ch.njol.skript.lang.ParseContext
 import ch.njol.skript.registrations.Classes
+import com.infernalsuite.aswm.api.world.properties.SlimeProperties
 import com.infernalsuite.aswm.api.world.properties.SlimePropertyMap
 
 class Types {
@@ -25,6 +28,34 @@ class Types {
 			Classes.registerClass(
 				ClassInfo(SlimePropertyMap::class.java, "slimepropertymap")
 					.since("1.0.0")
+					.parser(object : Parser<SlimePropertyMap>() {
+						override fun canParse(context: ParseContext?): Boolean {
+							return false
+						}
+
+						override fun toString(slimePropertyMap: SlimePropertyMap?, flags: Int): String {
+							return toVariableNameString(slimePropertyMap)
+						}
+
+						override fun toVariableNameString(slimePropertyMap: SlimePropertyMap?): String {
+							return slimePropertyMap?.let {
+								"SlimePropertyMap{" +
+										"spawnX=${it.getValue(SlimeProperties.SPAWN_X)}," +
+										"spawnY=${it.getValue(SlimeProperties.SPAWN_Y)}," +
+										"spawnZ=${it.getValue(SlimeProperties.SPAWN_Z)}," +
+										"spawnYaw=${it.getValue(SlimeProperties.SPAWN_YAW)}," +
+										"difficulty=${it.getValue(SlimeProperties.DIFFICULTY)}," +
+										"allowMonsters=${it.getValue(SlimeProperties.ALLOW_MONSTERS)}," +
+										"allowAnimals=${it.getValue(SlimeProperties.ALLOW_ANIMALS)}," +
+										"dragonBattle=${it.getValue(SlimeProperties.DRAGON_BATTLE)}," +
+										"pvp=${it.getValue(SlimeProperties.PVP)}," +
+										"environment=${it.getValue(SlimeProperties.ENVIRONMENT)}," +
+										"worldType=${it.getValue(SlimeProperties.WORLD_TYPE)}," +
+										"defaultBiome=${it.getValue(SlimeProperties.DEFAULT_BIOME)}" +
+										"}"
+							} ?: "SlimePropertyMap is null"
+						}
+					})
 			)
 		}
 	}
